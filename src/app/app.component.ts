1ea8c8c2ab7d7e57170632729d104ed09df67160
thiago <tguedes6@hotmail.com>
2021-09-04 18:34:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// import './assets/sass/main.scss';
+// import '../assets/sass/main.scss';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-angular';
+
+  myThead = [
+    'Sigla',
+    'Score',
+  ]
+
+  myBody = [
+    ['ABC', '0.2'],
+    ['ABC', '0.2'],
+    ['ABC', '0.2'],
+  ]
+
+  // testStyle = { paddingRight: '10px' }
+  // marginBottom = { marginBottom: '32px' }
+  // marginBottom16 = { marginBottom: '16px' }
+  marginTop = { marginTop: '64px' }
+
+  fix = { marginBottom: '32px', position: 'sticky', top: '10px' }
+
+  styleCard = {
+    padding: '18px',
+    textAlign: 'center'
+  }
+
+  styleCardMiddle = {
+    ...this.styleCard,
+    borderRadius: '0px'
+  }
+
+  styleCardSide = {
+    ...this.styleCard
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { AngularLibModule } from 'lib-angular';
+// import { AngularLibModule } from 'AngularLibModule';
+// import { AngularLibModule } from 'lib-angular/angular-lib';
+// import { AngularLibModule } from 'lib-angular/projects/angular-lib/src/public-api';
+// import { AngularLibModule } from 'lib-angular/projects/angular-lib/src/lib/angular-lib.module';
+
+import { AngularLibModule, CircleProgressModule } from 'lib-angular/dist/angular-lib';
+// import { AngularLibComponent } from 'lib-angular/dist/angular-lib';
+// import { AngularLibComponent } from 'lib-angular/projects/angular-lib/src/public-api';
+
+import { TestLibComponent } from './test-lib/test-lib.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestLibComponent,
+    // AngularLibComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularLibModule,
+    CircleProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  // bootstrap: [TestLibComponent]
 })
 export class AppModule { }
